b188187b21dd25745fe857ca177d5fe07eb93000
Jimmy Bobberson <mozambaniac@gmail.com>
2026-05-08 16:35:02 -0700

    Initial commit 2
    Add files and  gitignore cuz i forgot to make this a repo

---
+++ PolyEx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace Flexpressions {
+	public struct MonoEx<T> where T : INumber<T> {
+
+		static private readonly char DEFAULT_IV = 'x';
+		static private readonly T DEFAULT_CO = T.One;
+		static private readonly Dictionary<T, string> EXPONENT_FOR = new Dictionary<T, string>() {
+
+			{T.Zero, "\u2070"},
+			{T.One, "\u00B9"},
+			{T.CreateChecked(2), "\u00B2"},
+			{T.CreateChecked(3), "\u00B3"},
+			{T.CreateChecked(4), "\u2074"},
+			{T.CreateChecked(5), "\u2075"},
+			{T.CreateChecked(6), "\u2076"},
+			{T.CreateChecked(7), "\u2077"},
+			{T.CreateChecked(8), "\u2078"},
+			{T.CreateChecked(9), "\u2079"}
+
+		};
+
+		char independent;
+		T coefficient;
+		T degree; 
+
+		public MonoEx(T degree){
+
+			coefficient = DEFAULT_CO;
+			independent = DEFAULT_IV;
+			this.degree = degree;
+
+		}
+
+		public string Expression => "" + independent + coefficient + degree
+
+
+
+	}
+
+	class PolyEx<T> where T : INumber<T>{
+
+		
+
+	}
+
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Flexpressions {
+
+	class Program {
+
+		static void Main(string[] args) {
+
+			Console.WriteLine("hi");
+			Console.WriteLine("hi");
+
+			string? plop = Console.ReadLine();
+
+			if (plop != null)
+				Console.WriteLine(plop);
+
+		}
+
+	}
+}
