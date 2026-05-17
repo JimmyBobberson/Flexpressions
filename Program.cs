d287fae4c2ea898cb6b02a07ff9caa5e2b30ecb5
Jimmy Bobberson <mozambaniac@gmail.com>
2026-05-16 17:29:53 -0700

    Update gitignore

--- Program.cs
+++ Program.cs
@@ -8,11 +8,6 @@ namespace Flexpressions;
 // todo: implement "complex functions" which can have trig, log, exponent, rational, etc (but do not have derivative)
 // todo: advanced functions with composition and chaining
 
-// todo for legacy:
-//		add updated CompareTo logic
-//		add refactored Flex
-//		add function variable caching
-
 class Program {
 
 	static void Main(string[] args) {
